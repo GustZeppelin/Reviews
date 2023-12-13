e58e06dfc4fda1c0009d0eca0ea060e2852582c0
unknown <gustzeppelin@gmail.com>
2023-12-13 18:28:02 -0300

    Finalização do html, algumas alterações CSS e inicio do JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+image = document.querySelector(".client-img");
+clientName = document.querySelector("h1");
+product = document.querySelector("h2");
+review = document.querySelector("p");
+prev = document.querySelector(".prev");
+next = document.querySelector(".next");
+counter = 0;
+
+objReviews = [
+    {image: "pessoa_1", name:"Joana", product:"pintão", client_review:"pintão gostoso"},
+    {image: "pessoa_2", name:"Mary", product:"pintão", client_review:"pintão gostoso"},
+    {image: "pessoa_3", name:"Will", product:"pintão", client_review:"pintão gostoso"}
+]
+
+
+
+console.log(objReviews[1])
